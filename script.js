315b8402aeb38d9c2baea521a380dc5efb87e89e
Emerge <egreengirl1@gmail.com>
2022-01-21 17:40:43 -0700

    html and styling for initial load of webpage

---
+++ script.js
@@ -0,0 +1,16 @@
+/* When the user clicks the Start Quiz button
+1. The first quiz question with multiple choice options will appear
+    user will be able to select from four options: A B C D
+    once the user selects an option and clicks submit button 
+        if the answer matches the correct options the page will show that the answer was correct
+        if the answer does not match the correct option the page will show that the answer was wrong
+        the screen automatically displays the next question for the user to answer
+2. The timer in the top right corner and start counting down from 75s 
+3. after 10 questions the
+
+*/
+
+
+
+
+
